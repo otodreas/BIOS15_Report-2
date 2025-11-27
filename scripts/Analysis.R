03f328240c4346a36029959172999a0cdf4162c0
otodreas <159846818+otodreas@users.noreply.github.com>
2025-11-27 16:52:47 +0100

    continued work on Analysis.R

--- scripts/Analysis.R
+++ scripts/Analysis.R
@@ -7,14 +7,66 @@ library(MASS)
 # Import data
 df <- read.csv(here("data", "Eulaema.csv"))
 
-# Create model
+# Initial tests
+c(head(unique(df$Eulaema_nigrita), 10), tail(unique(df$Eulaema_nigrita), 10))  # Show top and bottom 10 unique entries
+hist(df$Eulaema_nigrita)  # Plot histogram of response variable
+lm <- lm(
+    df$Eulaema_nigrita
+    ~ df$effort
+    + df$altitude
+    + df$MAT
+    + df$MAP
+    + df$Tseason
+    + df$Pseason
+    + df$forest.
+    + df$lu_het
+)  # Fit simple linear model for all numerical predictors
+hist(lm$residuals)  # Show distribution of residuals
+
+plm <- glm(
+    df$Eulaema_nigrita
+    ~ df$effort
+    + df$altitude
+    + df$MAT
+    + df$MAP
+    + df$Tseason
+    + df$Pseason
+    + df$forest.
+    + df$lu_het
+)  # Fit multiple poisson regression model
+summary(plm)  # Check if data exhibit overdispersion
+
+nblm <- glm.nb(
+    df$Eulaema_nigrita
+    ~ df$effort
+    # + df$altitude
+    # + df$MAT
+    + df$MAP
+    + df$Tseason
+    + df$Pseason
+    + df$forest.
+    # + df$lu_het
+)  # Fit multiple negative binomial regression to the data
+# Check summary
 
 # Drop terms that I do not expect have an important relationship to the response
 
 # Check that residuals are normally distributed
 
 # Run a multicollinearity check, drop correlated variables
+vars <- list(
+    df$effort, df$MAP, df$Tseason, df$Pseason, df$forest.
+)  # Create list of variables
+# par(mfrow = rep(length(vars), 2))  # Create plot grid
+for (v_x in vars) {  # Loop through variables
+  for (v_y in vars) {
+    if (i > j) {
+      
+    } plot(vars[[i]], vars[[j]]) 
+  }
+}
+# Plot each unique pair of variables
 
 # Produce a table with parameter estimates, etc.
 
-# Produce plot(s)
+  # Produce plot(s)
